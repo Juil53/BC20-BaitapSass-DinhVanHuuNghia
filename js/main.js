b164567edf3488282ef19914141d0d07e3bb2883
Đinh Văn Hữu Nghĩa <86728246+Juil53@users.noreply.github.com>
2021-11-10 22:50:15 +0700

    update

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ window.onscroll = function() {
 function scrollFunction(){
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
         document.getElementById("header").style.backgroundColor = "white";
-        document.getElementById("header").style.padding = "10px 5px";
+        document.getElementById("header").style.padding = "5px 5px";
         if (window.innerWidth <= 992)
         document.getElementById("wavylogo").style.display = "block";
         document.querySelector(".navbar-toggler").style.top = "10px";
+        document.getElementById("header").style.padding = "0px 0px";
     } else {
         document.getElementById("header").style.backgroundColor = "transparent";
         document.getElementById("header").style.padding = "0px 0px";
